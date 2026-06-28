132f38037f547c0de06f0c51786711afdb9745df
akashneelGhoshDev <akashneel.ghosh.enterprises@gmail.com>
2026-06-28 23:28:35 +0530

    feat: enhance profile layout with detailed skills and work experience, add contact icons

--- personal-portfolio-ui/src/app/features/layout/layout/layout.ts
+++ personal-portfolio-ui/src/app/features/layout/layout/layout.ts
@@ -1,10 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatDividerModule } from '@angular/material/divider';
 import { AppDataService } from '../../../core/services/app-data/app-data.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-layout',
-  imports: [MatCardModule],
+  imports: [MatCardModule, MatChipsModule, MatDividerModule, CommonModule],
   templateUrl: './layout.html',
   styleUrl: './layout.scss',
 })
